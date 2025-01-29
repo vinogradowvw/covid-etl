2bc135f46083adcd116b4f9f5030b062fdc3fb98
vinogradowvw <vinogradov.gr.s@gmail.com>
2025-01-29 18:42:45 +0100

    using python instead of bash to download file

---
+++ Airflow/covid_etl/scripts/CSV_2020/extract.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def extract():
+
+    url = "https://drive.google.com/uc?id=18Q2VzN9nVZl9CD6a4kC3JoETe4QCxS3h"
+    output_path = "/opt/bitnami/airflow/dags/covid_etl/data/data_2020.csv"
+
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(output_path, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+    else:
+        print(f"Error: {response.status_code}")
