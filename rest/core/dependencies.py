678e13909dc385914970b5ff42739ef24f94a0ab
vinogradowvw <vinogradov.gr.s@gmail.com>
2025-02-01 22:52:36 +0100

    dependecies refactor

--- rest/core/dependencies.py
+++ rest/core/dependencies.py
@@ -1,23 +1,29 @@
-from clickhouse_connect import get_async_client
-from clickhouse_connect.driver.asyncclient import AsyncClient
-import os
+from contextlib import asynccontextmanager
+from fastapi import Depends, FastAPI
+from repositories import Repository
+from core.clickhouse_client import get_client
+from services import Service
 
 
-async def get_client() -> AsyncClient:
+repository = Repository(client=Depends(get_client))
 
-    ch_host: str = 'localhost' if os.getenv('CH_HOST') is None else str(os.getenv('CH_HOST'))
-    if (os.getenv('CH_PORT') is None):
-        ch_port: int = 8123
-    elif (os.getenv('CH_HOST').isnumeric()):
-        ch_port: int = int(os.getenv('CH_HOST'))
-    else:
-        raise TypeError("ClickHouse port environment variable must be a integer!")
 
-    ch_user: str = 'default' if os.getenv('CH_USER') is None else str(os.getenv('CH_USER'))
-    ch_pass: str = '' if os.getenv('CH_PASSWORD') is None else str(os.getenv('CH_PASSWORD'))
+def get_repository():
+    return repository
 
-    client: AsyncClient = await get_async_client(port=ch_port,
-                                                 host=ch_host,
-                                                 user=ch_user,
-                                                 password=ch_pass)
-    return client
+
+service = Service(repository=Depends(get_repository))
+
+
+def get_service():
+    return service
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    app.state.client = get_repository().client
+
+    yield
+
+    await app.state.client.close()
